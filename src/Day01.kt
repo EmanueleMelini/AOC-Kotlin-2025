84db1f0167b74455119c757c9baa0dd9b6004fcc
EmanueleMelini <meliniemanuele@gmail.com>
2025-12-02 17:44:04 +0100

    Day01 completed. Brute forced because I have no time to live.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,37 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var timesAt0 = 0
+        var currentPos = 50
+        for (rotation in input) {
+            val rotationValue = rotation.replace("[^0-9]".toRegex(), "").toInt() * if (rotation.replace("[0-9]".toRegex(), "") == "L") -1 else 1
+            currentPos += rotationValue
+            if (currentPos < -99) currentPos %= 100
+            if (currentPos < 0) currentPos = 100 - abs(currentPos)
+            if (currentPos > 99) currentPos %= 100
+            if (currentPos == 0) timesAt0++
+        }
+        return timesAt0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var timesAt0 = 0
+        var currentPos = 50
+        for (rotation in input) {
+            val rotationValue = rotation.replace("[^0-9]".toRegex(), "").toInt()
+            val rotationDir = rotation.replace("[0-9]".toRegex(), "") == "L"
+            (1..rotationValue).forEach { _ ->
+                if (rotationDir) currentPos-- else currentPos++
+                if (currentPos < -99) currentPos %= 100
+                if (currentPos < 0) currentPos = 100 - abs(currentPos)
+                if (currentPos > 99) currentPos %= 100
+                if (currentPos == 0) timesAt0++
+            }
+        }
+        return timesAt0
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
